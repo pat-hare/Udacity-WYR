ae492b75c05ac15f75e1fec691fe3a7ca2e7d587
Patrick Hare <patrickhare42@gmail.com>
2018-05-24 12:30:51 +0100

    Initial Commit w/ handleInitialData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { handleInitialData } from './actions/index'
 
 class App extends Component {
   render() {
+    console.log('InitialData:  ', handleInitialData())
     return (
       <div>
         Hello World
