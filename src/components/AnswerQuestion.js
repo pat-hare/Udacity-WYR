173e3b2879781f462b0790a3925ff95de781f509
Patrick Hare <patrickhare42@gmail.com>
2018-05-31 12:57:32 +0100

    Bug fixes

--- src/components/AnswerQuestion.js
+++ src/components/AnswerQuestion.js
@@ -4,39 +4,58 @@ import * as Actions from '../actions/questions'
 import { bindActionCreators } from 'redux'
 
 class AnswerQuestion extends Component {
+  state = {
+    answered: false,
+    classOne: 'notActive',
+    classTwo: 'notActive',
+  }
   render() {
     const { questionID, setUser } = this.props
     const { optionOne, optionTwo } = this.props.questions[questionID]
     const optionOneStr = 'optionOne'
     const optionTwoStr = 'optionTwo'
     console.log(this.props)
+    console.log(this.state)
     return (
       <div>
         <div>
           <h2>{optionOne.text}</h2>
+          {this.state.answered || this.props.question.optionOne.votes.includes(setUser) || this.props.question.optionTwo.votes.includes(setUser)
+            ? <p className={this.state.classOne}>{this.props.question.optionOne.votes.length} votes</p>
+            : <p></p>}
         </div>
         <button
           className='loginButton'
-          onClick={() => this.props.actions.handleAnswerQuestion({authedUser: setUser, answer: optionOneStr, qid: questionID}) && this.props.history.push('/home')}>
+          onClick={() => this.props.actions.handleAnswerQuestion({authedUser: setUser, answer: optionOneStr, qid: questionID}) && this.setState({ answered: true, classOne: 'active' })}
+          disabled={this.state.answered || this.props.question.optionOne.votes.includes(setUser) || this.props.question.optionTwo.votes.includes(setUser)}>
           X
         </button>
+
         <span>
           <h3>OR</h3>
         </span>
+
         <div>
           <h2>{optionTwo.text}</h2>
+          {this.state.answered || this.props.question.optionOne.votes.includes(setUser) || this.props.question.optionTwo.votes.includes(setUser)
+            ? <p className={this.state.classTwo}>{this.props.question.optionTwo.votes.length} votes</p>
+            : <p></p>}
         </div>
         <button
           className='loginButton'
-          onClick={() => this.props.actions.handleAnswerQuestion({authedUser: setUser, answer: optionTwoStr, qid: questionID}) && this.props.history.push('/home')}>
+          onClick={() => this.props.actions.handleAnswerQuestion({authedUser: setUser, answer: optionTwoStr, qid: questionID}) && this.setState({ answered: true, classTwo: 'active' })}
+          disabled={this.state.answered || this.props.question.optionOne.votes.includes(setUser) || this.props.question.optionTwo.votes.includes(setUser)}>
           X
         </button>
+
         <span>
           <h3>BY</h3>
         </span>
+
         <div>
           <img src={this.props.author.avatarURL} alt={`Avatar for ${this.props.author.name}`}/>
         </div>
+
       </div>
     )
   }
@@ -45,13 +64,14 @@ class AnswerQuestion extends Component {
 function mapStateToProps ({ questions, users, setUser }) {
   const URLPath = window.location.pathname.split('/')
   const questionID = URLPath[2]
-  const authorQ = questions[questionID]
-  const author = users[authorQ.author]
+  const questionState = questions[questionID]
+  const author = users[questionState.author]
   console.log(author)
   return {
     questionID: questionID,
     questions: questions,
     author: author,
+    question: questionState,
     users,
     setUser,
 
